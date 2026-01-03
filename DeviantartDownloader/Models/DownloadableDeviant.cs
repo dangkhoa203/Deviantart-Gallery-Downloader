5d79a132d4850d6c04d35f13f90d4b0ad3624871
trankhoa515 <trankhoa515@gmail.com>
2026-01-04 00:57:52 +0700

    Fix status

--- DeviantartDownloader/Models/DownloadableDeviant.cs
+++ DeviantartDownloader/Models/DownloadableDeviant.cs
@@ -5,17 +5,43 @@ using System.Text;
 
 namespace DeviantartDownloader.Models
 {
-    public class DownloadableDeviant
+    public class DownloadableDeviant:ObservableObject
     {
+        private float _percent;
+        public float Percent {
+            get { return _percent; }
+            set {
+                _percent = value;
+                OnPropertyChanged(nameof(Percent));
+            }
+        }
+        public DownloadStatus _status;
+        public DownloadStatus Status {
+            get { return _status; }
+            set {
+                _status = value;
+                OnPropertyChanged(nameof(Status));
+            }
+        }
+
+        private string _downloadSpeed;
+        public string DownloadSpeed {
+            get { return _downloadSpeed; }
+            set {
+                _downloadSpeed = value;
+                OnPropertyChanged(nameof(DownloadSpeed));
+            }
+        }
+
         public Deviant Deviant { get; set; }
-        public float Percent { get; set; }
-        public DownloadStatus Status { get; set; }
-        public string DownloadSpeed { get; set; }
+        
+      
+       
         public DownloadableDeviant(Deviant deviant) {
             Deviant = deviant;
-            Percent = 0;
-            Status=DownloadStatus.Waiting;
-            DownloadSpeed = "";
+            _percent = 0;
+            _status = DownloadStatus.Waiting;
+            _downloadSpeed = "";
         }
         public bool? IsSelected { get; set; } = false;
     }
